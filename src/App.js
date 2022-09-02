d92369640f8fcfea3df51969599f874877b5315c
j-eduardosandoval <eduardosandoval.atc@gmail.com>
2022-09-02 11:23:42 -0500

    Login user maquetacion

--- src/App.js
+++ src/App.js
@@ -4,20 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Laboratorio Teso
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+      <header className="App-header">        
       </header>
+      <main>
+        <div className='container'>
+        <p>
+          Welcome to Laboratorio Teso de México
+        </p>
+        <div className='container-login'>
+            <div className='header-login'>              
+            </div>
+            <div className='main-login'>
+               <form>
+                <div className='login-user'>
+                <label>Usuario: </label>
+                  <input type="text" name='usename' />
+                </div>
+                <div className='login-password'>
+                <label>Password: </label>
+                  <input type="Password" name='usepass' />
+                </div>
+                <div className='container-button-login'>
+                  <input type="submit" />
+                </div>
+               </form>
+            </div>
+            <div className='foother-login'>
+            </div>
+        </div>
+        </div>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
